4180d9c094fa49314fcdad6bff0dd0514f384115
FiorellaQuijano <mfalvare@espol.edu.ec>
2023-10-25 16:34:36 -0500

    Solucionando Violacion Open/Closed

--- Taller Solid/src/CloudServicePlatform.java
+++ Taller Solid/src/CloudServicePlatform.java
@@ -1,17 +1,35 @@
 public class CloudServicePlatform {
     private int cloudServicePlatform;
+    Platform[] plataformas = { new Aws(),new MicrosoftAzure(),new GoogleCloud()};
     public void hostingTo (AppWeb app) {
-        if (cloudServicePlatform==1) {
-            System.out.println("Connect to AWS");
-            // Logic
-        }if (cloudServicePlatform==2) {
-            System.out.println("Connect to Microsoft Azure");
-            // Logic
-        }else{
-            System.out.println("Connect to Google Cloud");
-            // Logic
-        }
+        System.out.println("Connect to" + plataformas[cloudServicePlatform+1].nombre());
     }
+
     // More Methods
 }
+abstract class Platform{
+    abstract String nombre();
 
+}
+class Aws extends Platform{
+
+
+    @Override
+    String nombre() {
+        return "AWS";
+    }
+}
+class MicrosoftAzure extends Platform{
+
+    @Override
+    String nombre() {
+        return "Microsoft Azure";
+    }
+}
+class GoogleCloud extends Platform{
+
+    @Override
+    String nombre() {
+        return "Google Cloud";
+    }
+}
